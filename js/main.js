eb243520d514d73eb6d9fb2cb8ef53229e81cbdd
Kate Grytsenko <aktinijal@gmail.com>
2016-09-29 11:10:42 +0300

    socials and preloader

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
+$(window).on('load', function () {
+    var $preloader = $('#page-preloader'),
+        $spinner   = $preloader.find('.spinner');
+    $spinner.fadeOut();
+    $preloader.delay(350).fadeOut('slow');
+    
+});
 $(document).ready(function () {
 
     //Меню навігації
